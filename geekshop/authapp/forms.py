74ccc01a2a389d7702ee91d3c019384d610ca3c4
Igor <rick_id@mail.com>
2020-12-24 18:01:02 +0300

    1. Реализовать в проекте простой вариант аутентификации пользователя через социальную сеть Google+. 2. Поработать со связью моделей «один-к-одному»: создать профиль пользователя и обеспечить возможность его редактирования. 3. Реализовать автоматическое заполнение профиля пользователя при аутентификации через социальную сеть. 4. Проверить работу исключения «AuthForbidden», например, задав при проверке минимальный возраст 100 лет.

--- geekshop/authapp/forms.py
+++ geekshop/authapp/forms.py
@@ -5,7 +5,7 @@ import django.forms as forms
 
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
-from authapp.models import ShopUser
+from authapp.models import ShopUser, ShopUserProfile
 
 
 class ShopUserLoginForm(AuthenticationForm):
@@ -65,4 +65,14 @@ class ShopUserEditForm(UserChangeForm):
             raise forms.ValidationError("Вам меньше 18 лет")
         return data
 
+
+class ShopUserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = ShopUserProfile
+        fields = ('tagline', 'aboutMe', 'gender')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 
-from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm, ShopUserProfileEditForm
 from authapp.models import ShopUser
 
 
@@ -72,10 +72,13 @@ def register(request):
 def edit(request):
     if request.method == 'POST':
         edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
-        if edit_form.is_valid():
+        profile_edit_form = ShopUserProfileEditForm(request.POST, instance=request.user.shopuserprofile)
+
+        if edit_form.is_valid() and profile_edit_form.is_valid():
             edit_form.save()
             return HttpResponseRedirect(reverse('authapp:edit'))
     else:
         edit_form = ShopUserEditForm(instance=request.user)
-    content = {'edit_form': edit_form}
+        profile_edit_form = ShopUserProfileEditForm(instance=request.user.shopuserprofile)
+    content = {'edit_form': edit_form, 'profile_form': profile_edit_form}
     return render(request, 'authapp/edit.html', content)
