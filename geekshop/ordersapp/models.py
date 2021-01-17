edea17ab86350721929f55f67e368daa9f01c7d3
Igor <rick_id@mail.com>
2021-01-17 20:53:53 +0300

    добавляем индексы

--- geekshop/ordersapp/models.py
+++ geekshop/ordersapp/models.py
@@ -27,7 +27,7 @@ class Order(models.Model):
                               max_length=3,
                               choices=ORDER_STATUS_CHOICES,
                               default=FORMING)
-    is_active = models.BooleanField(verbose_name='активен', default=True)
+    is_active = models.BooleanField(verbose_name='активен', default=True, db_index=True)
 
     def get_total_quantity(self):
         items = self.orderitems.select_related()
