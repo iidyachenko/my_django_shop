940481a2adf9ee02cd3f89c2505cc14b12e65882
Igor <rick_id@mail.com>
2021-01-19 19:50:38 +0300

    кеширование контролера с продуктами

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.decorators.cache import cache_page
 
 import mainapp.views as mainapp
 
@@ -9,7 +10,7 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.products, name='index'),
-    path('<int:pk>/', mainapp.products, name='category'),
+    path('<int:pk>/', cache_page(3600)(mainapp.products), name='category'),
     path('<int:pk>/<int:page>/', mainapp.products, name='page'),
     path('product/<int:pk>/', mainapp.product, name='product')
 ]
