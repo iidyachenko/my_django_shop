ffe2e1bd96b060176efa7db817f690c52561218c
Igor <rick_id@mail.com>
2021-01-20 17:14:05 +0300

    кеширование контролера с продуктами

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.decorators.cache import cache_page
 
 import mainapp.views as mainapp
@@ -11,9 +11,9 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', mainapp.products, name='index'),
     path('<int:pk>/', cache_page(3600)(mainapp.products), name='category'),
-    path('<int:pk>/ajax/', mainapp.products_ajax, name='category'),
+    re_path(r'^category/(?P<pk>\d+)/page/(?P<page>\d+)/ajax/$', cache_page(3600)(mainapp.products_ajax)),
     path('<int:pk>/<int:page>/', cache_page(3600)(mainapp.products), name='page'),
-    path('<int:pk>/<int:page>/ajax/', mainapp.products_ajax, name='page'),
+    re_path(r'^category/(?P<pk>\d+)/ajax/$', cache_page(3600)(mainapp.products_ajax)),
     path('product/<int:pk>/', mainapp.product, name='product')
 
 ]
--- geekshop/static/js/main_scripts.js
+++ geekshop/static/js/main_scripts.js
@@ -1,6 +1,8 @@
 $( document ).on( 'click', '.details a', function(event) {
    if (event.target.hasAttribute('href')) {
        var link = event.target.href + 'ajax/';
+       var link_array = link.split('/');
+       if (link_array[4] === 'category') {
            $.ajax({
                url: link,
                success: function (data) {
@@ -8,5 +10,7 @@ $( document ).on( 'click', '.details a', function(event) {
                }
            });
 
+           event.preventDefault();
+       }
    }
 });
