0ccdfbd9a33952d4d7685df4e66761c559e8f5c3
Igor <rick_id@mail.com>
2021-01-20 17:39:31 +0300

    кеширование контролера с продуктами

--- geekshop/static/js/main_scripts.js
+++ geekshop/static/js/main_scripts.js
@@ -2,6 +2,7 @@ $( document ).on( 'click', '.details a', function(event) {
    if (event.target.hasAttribute('href')) {
        var link = event.target.href + '/ajax/';
        var link_array = link.split('/');
+       console.log(link)
        console.log(link_array)
        console.log(link_array[4])
        if (link_array[3] === 'products') {
